c8159a56aa9ca470e35638201c499990e9138288
yijian <2269260331@qq.com>
2022-08-29 22:07:07 +0800

    1

--- src/main/unitTest.cpp
+++ src/main/unitTest.cpp
@@ -4,12 +4,15 @@
 #include "pch.h"
 #include "Complex.hpp"
 #include "traits.hpp"
+#include <vector>
+
+using namespace std;
 
 double complexTest()
 {
     Complex<double> a, b(1, 2), c(3, 4);
     a = b + c; // a = 4 6
-    std::cout << a << std::endl << std::is_same<Complex<int>, Complex<double>>::value <<std::endl;
+    cout << a << std::endl << is_same<Complex<int>, Complex<double>>::value <<endl;
     return a.getRE();
 }
 
@@ -20,7 +23,23 @@ double traitsTest()
     return 0;
 }
 
+double bing2ndTest()
+{
+    vector<int> v;
+    v.push_back(2);
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(9);
+    v.push_back(7);
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    cout << count_if(v.begin(), v.end(), bind2nd(less<int>(), 5) ) << endl;
+    return 1;
+}
+
 TEST_CASE("Test") {
-    REQUIRE(complexTest() == 4);
-    REQUIRE(traitsTest() == 0);
+    REQUIRE(bing2ndTest() == 1);
+    // REQUIRE(complexTest() == 4);
+    // REQUIRE(traitsTest() == 0);
 }
